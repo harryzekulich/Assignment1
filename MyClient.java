1ad180079d7b04612bb380757c5e548ea76ff91c
harryzekulich <100758855+harryzekulich@users.noreply.github.com>
2022-03-30 19:07:25 +1100

    Trying new way of updating client

--- MyClient.java
+++ MyClient.java
@@ -1,84 +1,95 @@
-import java.io.*;
+iimport java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 
 public class MyClient {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 50000);
+      
+            String[] biggestServer = {""};
+            boolean biggestFound = false;
+            String currentMsg = "";
+            handshake(s);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-    private static final String HELO = "HELO\n";
-    private static final String AUTH = "AUTH USER\n";
-    private static final String REDY = "REDY\n";
-    private static final String QUIT = "QUIT\n";
-  
-
-    /*
-     * public static void main(String[] args) {
-     * try{
-     * Socket s=new Socket("localhost",50000);
-     * DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-     * dout.write(("HELO").getBytes());
-     * dout.flush();
-     * dout.close();
-     * s.close();
-     * }catch(Exception e){System.out.println(e);}
-     * }
-     * }
-     */
-
-    public static void main(String args[]) throws Exception {
-        Socket s = new Socket("localhost", 50000);
-        DataInputStream din = new DataInputStream(s.getInputStream());
-        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        
-
-        dout.write((HELO).getBytes());
-        dout.flush();
-
-        System.out.println(din.readLine());
-
-        dout.write((AUTH).getBytes());
-        dout.flush();
-        
-        System.out.println(din.readLine());
-
-        dout.write((REDY).getBytes());
-        dout.flush();
-
-        String msg = din.readLine();
-        System.out.println(msg);
-
-        while(msg.equals("QUIT")){
             
-        }
-        
-        System.out.println(din.readLine());
+            while (!currentMsg.contains("NONE")) {
+              
+                sendMsg(s, "REDY\n");
+                currentMsg = readMsg(s);
+                
+                
+                if (currentMsg.contains("JOBN")) {
+                    String[] JOBNSplit = currentMsg.split(" ");
+                  
+                    sendMsg(s, "GETS Avail " + JOBNSplit[4] + " " + JOBNSplit[5] + " " + JOBNSplit[6] + "\n");
+ 
+                    currentMsg = readMsg(s);
+                    sendMsg(s, "OK\n");
 
-        dout.write((QUIT).getBytes());
-        dout.flush();
-        
-        
-        /*
-        String str = "", str2 = "";
-        while (!str.equals("stop")) {
-            str = br.readLine();
-            dout.write(("HELO").getBytes());
+                   
+                    currentMsg = readMsg(s);
+                    sendMsg(s, "OK\n");
+
+                    if(biggestFound == false){
+                        biggestServer = findBiggestServer(currentMsg);
+                        biggestFound = true;
+                    }
+
+                    currentMsg = readMsg(s);
+
+                    sendMsg(s, "SCHD " + JOBNSplit[2] + " " + biggestServer[0] + " " + biggestServer[1] + "\n");
+
+                    currentMsg = readMsg(s);
+                    System.out.println("SCHD: " + currentMsg);
+                }
+                else if (currentMsg.contains("DATA")) {
+                    sendMsg(s, "OK\n");
+                }
+            }
+            sendMsg(s, "QUIT\n");
+            s.close();
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    
+     public static synchronized String readMsg(Socket s) {
+        String currentMsg = "FAIL";
+        try {
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            byte[] byteArray = new byte[dis.available()];
+      
+            byteArray = new byte[0];
+            while (byteArray.length == 0) {
+             
+                byteArray = new byte[dis.available()];
+                dis.read(byteArray);
+               
+                currentMsg = new String(byteArray, StandardCharsets.UTF_8);
+                
+                ublic static synchronized void sendMsg(Socket s, String currentMsg) {
+        try {
+
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            byte[] byteArray = currentMsg.getBytes();
+            dout.write(byteArray);
             dout.flush();
-            str2 = din.readUTF();
-            System.out.println("Server says: " + str2);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+                      public static void handshake(Socket s) {
+        String currentMsg = "";
 
-        */
+     sendMsg(s, "HELO\n");
 
-
-
-
-        dout.close();
-        s.close();
+        currentMsg = readMsg(s);
+        System.out.println("RCVD: " + currentMsg);
+        sendMsg(s, "AUTH " + System.getProperty("user.name") + "\n");
+        currentMsg = readMsg(s);
+        System.out.println("RCVD: " + currentMsg);
     }
-
-    public void GetsCapable (){
-
-    }
-}
-
-public void findBiggest(){
-
-}
