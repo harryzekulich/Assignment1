1c34970e4c12f15fdabf831551c4e8b71d8d4cfe
harryzekulich <100758855+harryzekulich@users.noreply.github.com>
2022-05-30 12:44:28 +1000

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -7,88 +7,81 @@ public class Scheduler {
         this.serverList = serverList;
     }
 
-    // Determine which scheduling method to be used based on the command line
-    // argument
+  //choose a scheduler algorithm stage 2 = NEW
+    
     public String schedule(String algorithm) {
         switch (algorithm) {
             case "ATL":
                 return allToLargest();
             case "NEW":
                 return newAlgorithm();
-            default: // When argument provided to client was not ATL or NEW, exit program and print
-                     // statement to let user know
+            default: 
                 System.out.println("Please input a valid algorithm");
                 System.exit(1); // Exit program
-                return null; // To satisfy the function needs of a return statement
+                return null;
         }
     }
+    
+     // finds servers with least amount of cpus
+    private Server getSmallestServer(ArrayList<Server> serverList) {
+        Server smallest = serverList.get(0); //assigned as first in list
+        Server test;
 
-    // Simple algorithm to schedule all the jobs to the first largest server
-    public String allToLargest() {
-        Server largest = getLargestServer(serverList); // Finds the largest server within the current serverList and
-                                                       // saves it
+        // again checks for compariosn
+        for (int i = 0; i < serverList.size() - 1; i++) {
+            test = serverList.get(i + 1); //test becomes 1 infront
+            if (smallest.getCoreCount() > test.getCoreCount()) { //sees if server has less cpus than previous
+                smallest = test; //if true, that becomes smallest
+            }
+        }
+        return smallest; //returns smallest server
+    }
+    
+    // finds servers with most amount of cores
+    private Server getLargestServer(ArrayList<Server> serverList) {
+        Server largest = serverList.get(0); //assigns largest server as first in list
+        Server test;
 
-        return largest.getServerTypeID(); // have the string prep for the SCHD message with the server details 
-                                          // (type and ID)
+        //check items for comparison
+        for (int i = 0; i < serverList.size() - 1; i++) {
+            test = serverList.get(i + 1); //test is 1 infront of loop for use 
+            if (largest.getCoreCount() < test.getCoreCount()) { // checks to see if server has more cpus
+                largest = test; //changes to largest if true
+            }
+        }
+        return largest; //largest server gets returned
     }
 
-    // Custom algorithm to schedule jobs to the server with the least amount of
-    // waiting jobs
+    // scheduler finds largest servers for jobs
+    public String allToLargest() {
+        Server largest = getLargestServer(serverList); //saves largest server from server list
+
+        return largest.getServerTypeID(); 
+    }
+
+   //the new algo which selects servers with least amount of jobs
     public String newAlgorithm() {
         Server best = shortestQueue(serverList); // Finds the server with the least amount of jobs
 
-        return best.getServerTypeID(); // have the string prep for the SCHD message with the server details (type and
-                                       // ID)
+        return best.getServerTypeID();
     }
 
-    // From within a ArrayList of servers, find the server with the most amount of
-    // cores
-    private Server getLargestServer(ArrayList<Server> serverList) {
-        Server largest = serverList.get(0); // Assign the largest server as the first item in the ArrayList
-        Server test;
+   
 
-        // Loop to check through every item in the ArrayList for comparison
-        for (int i = 0; i < serverList.size() - 1; i++) {
-            test = serverList.get(i + 1); // Set the server to check as one ahead of the loop
-            if (largest.getCoreCount() < test.getCoreCount()) { // See if the server being checked has more CPU cores
-                                                                // available
-                largest = test; // If so, set the largest server to the one just checked
-            }
-        }
-        return largest; // Return the server with the most CPU cores available
-    }
 
-    // Same as getLargestServer but insteads finds the server with the least amount
-    // of CPU cores available
-    private Server getSmallestServer(ArrayList<Server> serverList) {
-        Server smallest = serverList.get(0); // Assign the smallest server as the first item in the ArrayList
-        Server test;
-
-        // Loop to check through every item in the ArrayList for comparison
-        for (int i = 0; i < serverList.size() - 1; i++) {
-            test = serverList.get(i + 1); // Set the server to check as one ahead of the loop
-            if (smallest.getCoreCount() > test.getCoreCount()) { // See if the server being checked has less CPU cores
-                                                                 // available
-                smallest = test; // If so, set the smallest server to the one just checked
-            }
-        }
-        return smallest; // Return the server with the least amount of CPU cores available
-    }
-
-    // Finds the server with the least amount of waiting jobs present
+    // looks for server with smallest number of jobs
     private Server shortestQueue(ArrayList<Server> serverList) {
-        Server best = getSmallestServer(serverList); // Assign the best server as the first item in the ArrayList
+        Server best = getSmallestServer(serverList); // the best server is assigned as 1 in list
         Server test;
 
-        // Loop to check through every item in the ArrayList for comparison
+        //checks for comparison
         for (int i = 0; i < serverList.size() - 1; i++) {
-            test = serverList.get(i + 1); // Set the server to check as one ahead of the loop
-            if (best.getWaitingJobs() > test.getWaitingJobs()) { // See if the server being checked has less waiting
-                                                                 // jobs present
-                best = test; // If so, set the best server to the one just checked
+            test = serverList.get(i + 1); //set server to check 1 infront
+            if (best.getWaitingJobs() > test.getWaitingJobs()) { // sees if new server has less jobs
+                best = test; //if true change it
             }
         }
-        return best; // Return the server with the least amount of waiting jobs
+        return best; // Returns best server
     }
 
 }
